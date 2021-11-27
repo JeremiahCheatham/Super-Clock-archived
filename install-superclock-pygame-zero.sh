6dbbd18161402cb281264333420e1b92e3a8424d
JeremiahCheatham <72688505+JeremiahCheatham@users.noreply.github.com>
2021-11-27 18:23:14 +0000

    Update install-superclock-pygame-zero.sh

--- install-superclock-pygame-zero.sh
+++ install-superclock-pygame-zero.sh
@@ -8,6 +8,7 @@ mkdir -p /usr/share/superclock
 cp -r images /usr/share/superclock
 
 cp superclock-pygame-zero.py /usr/share/superclock
+chmod +x /usr/share/superclock/superclock-pygame-zero.py
 cp superclock-pygame-zero /usr/bin
 chmod +x /usr/bin/superclock-pygame-zero
 cp superclock-pygame-zero.desktop /usr/share/applications
