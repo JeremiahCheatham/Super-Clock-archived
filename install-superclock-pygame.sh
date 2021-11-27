9408486e82650e9c0c75eac532103810a2232455
JeremiahCheatham <72688505+JeremiahCheatham@users.noreply.github.com>
2021-11-27 18:22:16 +0000

    Update install-superclock-pygame.sh

--- install-superclock-pygame.sh
+++ install-superclock-pygame.sh
@@ -8,6 +8,7 @@ mkdir -p /usr/share/superclock
 cp -r images /usr/share/superclock
 
 cp superclock-pygame.py /usr/share/superclock
+chmod +x /usr/share/superclock/superclock-pygame.py
 cp superclock-pygame /usr/bin
 chmod +x /usr/bin/superclock-pygame
 cp superclock-pygame.desktop /usr/share/applications
