9094668cbd3b6964fd90c24fbfe0d143ab187b6b
JeremiahCheatham <72688505+JeremiahCheatham@users.noreply.github.com>
2021-11-27 18:19:59 +0000

    Update install-superclock-sdl.sh

--- install-superclock-sdl.sh
+++ install-superclock-sdl.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 if [ 0 != $(id -u) ]; then
-    echo "This script must be run as root with sudo.";
-    exit 1;
+    echo
+    echo "This script must be run as root with sudo."
+    echo
+    exit 1
+fi
+
+if ! make rebuild; then
+    echo
+    echo "Well that didn't work."
+    echo "To compile you need a basic biuld enviroment."
+    echo "You will also need SDL2 installed."
+    echo
+    exit 1
 fi
 
 if [ ! -f "superclock-sdl" ]; then
-    echo "superclock-sdl file not found.";
-    echo "Try running the make command to build superclock-sdl.";
-    exit 1;
+    echo
+    echo "superclock-sdl file not found."
+    echo "Try running the make command to build superclock-sdl."
+    echo
+    exit 1
 fi
 
 mkdir -p /usr/share/superclock
