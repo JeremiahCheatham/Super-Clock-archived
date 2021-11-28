32842a2d6e8405979596a87f9e028e7c0ab80ac8
JeremiahCheatham <72688505+JeremiahCheatham@users.noreply.github.com>
2021-11-28 23:01:31 +0000

    Update install-superclock-pygame-zero.sh

--- install-superclock-pygame-zero.sh
+++ install-superclock-pygame-zero.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ 0 != $(id -u) ]; then
+if [ 0 != $UID ]; then
     echo "This script must be run as root with sudo.";
     exit 1;
 fi
@@ -9,6 +9,6 @@ cp -r images /usr/share/superclock
 
 cp superclock-pygame-zero.py /usr/share/superclock
 chmod +x /usr/share/superclock/superclock-pygame-zero.py
-cp superclock-pygame-zero /usr/bin
-chmod +x /usr/bin/superclock-pygame-zero
+cp superclock-pygame-zero /usr/local/bin
+chmod +x /usr/local/bin/superclock-pygame-zero
 cp superclock-pygame-zero.desktop /usr/share/applications
